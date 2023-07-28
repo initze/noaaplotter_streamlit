4d30666c4bb580f33e3daf31f1e5c0ed20d1101f
initze <ingmarnitze@gmail.com>
2023-07-28 15:02:30 +0200

    minor refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,7 @@ import datetime
 from noaaplotter.noaaplotter import NOAAPlotter
 import os
 
-from utils import to_datestring, get_refperiod_from_widget, load_stations_from_pickle, date_to_datetime
-
-@st.cache_data()  # Set allow_output_mutation to True for caching mutable objects like dictionaries
-def load_data(dataset_selector, download_start, download_end, stations, API_TOKEN, station_name=None, coordinates_field=None):
-    if dataset_selector == 'NOAA station':
-        # get stations
-        station_id = stations[station_name]
-        data_file = f'NOAA_{station_id}.csv'
-        n_jobs = 4
-        try:
-            download_from_noaa(data_file, download_start, download_end, ['TMIN', 'TMAX', 'PRCP', 'SNOW'], station_name,
-                               station_id, API_TOKEN, n_jobs=n_jobs)
-        except:
-            n_jobs = 1
-            download_from_noaa(data_file, download_start, download_end, ['TMIN', 'TMAX', 'PRCP', 'SNOW'], station_name,
-                   station_id, API_TOKEN, n_jobs=n_jobs)
-    elif dataset_selector == 'ERA5':
-        """ERA5 data loading may take up to 5 minutes"""
-        lat, lon = coordinates_field.replace(' ', '').split(',')
-        data_file = f'ERA5_{lat}_{lon}.csv'
-        station_name = None
-        download_era5_from_gee(float(lat), float(lon), download_end, download_start, data_file)
-    return data_file, station_name
+from utils import *
 
 
 def main():
@@ -56,12 +34,10 @@ def main():
     # date container
     container_date = st.container()
     columns_date = container_date.columns(2)
-    today_datetime = datetime.datetime.today()
-    # Calculate one year before the current date
-    one_year_before = today_datetime.replace(year=today_datetime.year-1) + datetime.timedelta(days=1)
-    
+
     # Add one day to the calculated date
-    result_date = one_year_before + datetime.timedelta(days=1)
+    today_datetime, result_date = get_currentdate_and_lastyear
+    
     # Dates
     start = columns_date[0].date_input(("Start Date"), value=one_year_before, **kwargs_date_picker)
     end = columns_date[1].date_input(("End Date"), value=today_datetime, **kwargs_date_picker)
--- utils.py
+++ utils.py
@@ -47,3 +47,35 @@ def load_stations_from_pickle(link):
 
 def date_to_datetime(input_date):
     return datetime(input_date.year, input_date.month, input_date.day)
+
+
+@st.cache_data()  # Set allow_output_mutation to True for caching mutable objects like dictionaries
+def load_data(dataset_selector, download_start, download_end, stations, API_TOKEN, station_name=None, coordinates_field=None):
+    if dataset_selector == 'NOAA station':
+        # get stations
+        station_id = stations[station_name]
+        data_file = f'NOAA_{station_id}.csv'
+        n_jobs = 4
+        try:
+            download_from_noaa(data_file, download_start, download_end, ['TMIN', 'TMAX', 'PRCP', 'SNOW'], station_name,
+                               station_id, API_TOKEN, n_jobs=n_jobs)
+        except:
+            n_jobs = 1
+            download_from_noaa(data_file, download_start, download_end, ['TMIN', 'TMAX', 'PRCP', 'SNOW'], station_name,
+                   station_id, API_TOKEN, n_jobs=n_jobs)
+    elif dataset_selector == 'ERA5':
+        """ERA5 data loading may take up to 5 minutes"""
+        lat, lon = coordinates_field.replace(' ', '').split(',')
+        data_file = f'ERA5_{lat}_{lon}.csv'
+        station_name = None
+        download_era5_from_gee(float(lat), float(lon), download_end, download_start, data_file)
+    return data_file, station_name
+
+def get_currentdate_and_lastyear():
+    today_datetime = datetime.datetime.today()
+    # Calculate one year before the current date
+    one_year_before = today_datetime.replace(year=today_datetime.year-1) + datetime.timedelta(days=1)
+    # Add one day to the calculated date
+    result_date = one_year_before + datetime.timedelta(days=1)
+    
+    return today_datetime, result_date
